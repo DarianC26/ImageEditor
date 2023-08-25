b2bfcb9d05e1b7e89eb1bfc6c9dd5e8550fbf9e4
DarianC26 <dariancheung@gmail.com>
2023-08-25 01:11:53 -0400

    implemented contrast feature

--- src/imageeditor/NewJFrame.java
+++ src/imageeditor/NewJFrame.java
@@ -311,16 +311,60 @@ public class NewJFrame extends javax.swing.JFrame {
 
     private void BrightnessActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BrightnessActionPerformed
         // TODO add your handling code here:
-        RescaleOp rescaleOp = new RescaleOp(1.2f, 15, null);
-        bi = rescaleOp.filter(bi, bi);
+        BufferedImage brightness = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        for(int i =0; i < bi.getWidth(); i++) {
+            for(int j = 0; j < bi.getHeight(); j++) {
+                Color c = new Color(bi.getRGB(i,j));
+                int r = c.getRed() + 10;
+                int g = c.getGreen() + 10;
+                int b = c.getBlue() + 10;
+                int a = c.getAlpha();
+
+                if(r > 255) {
+                    r = 255;
+                }
+                if(g > 255) {
+                    g = 255;
+                }
+                if(b > 255) {
+                    b = 255;
+                }
+
+                Color gColor = new Color(r, g, b, a);
+                brightness.setRGB(i, j, gColor.getRGB());
+            }
+        }
+        bi = brightness;
         ii = new ImageIcon(bi);
         ImagePanel.setIcon(ii);
     }//GEN-LAST:event_BrightnessActionPerformed
-
+ 
     private void DarkenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DarkenActionPerformed
         // TODO add your handling code here:
-        RescaleOp rescaleOp = new RescaleOp(0.5f, 15, null);
-        bi = rescaleOp.filter(bi, bi);
+        BufferedImage darken = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        for(int i =0; i < bi.getWidth(); i++) {
+            for(int j = 0; j < bi.getHeight(); j++) {
+                Color c = new Color(bi.getRGB(i,j));
+                int r = c.getRed() - 10;
+                int g = c.getGreen() - 10;
+                int b = c.getBlue() - 10;
+                int a = c.getAlpha();
+
+                if(r < 0) {
+                    r = 0;
+                }
+                if(g < 0) {
+                    g = 0;
+                }
+                if(b < 0) {
+                    b = 0;
+                }
+
+                Color gColor = new Color(r, g, b, a);
+                darken.setRGB(i, j, gColor.getRGB());
+            }
+        }
+        bi = darken;
         ii = new ImageIcon(bi);
         ImagePanel.setIcon(ii);
     }//GEN-LAST:event_DarkenActionPerformed
@@ -333,12 +377,45 @@ public class NewJFrame extends javax.swing.JFrame {
 
     private void ResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ResetActionPerformed
         // TODO add your handling code here:
+        bi = originalImg;
         ii = new ImageIcon(originalImg);
         ImagePanel.setIcon(ii);
     }//GEN-LAST:event_ResetActionPerformed
 
     private void ContrastActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ContrastActionPerformed
         // TODO add your handling code here:
+    BufferedImage imageAltered = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_RGB);
+    int brightness = (int)(Math.random()*301) - 250; //values from 150 to 200
+    double contrast = 1.5 + (5.0 - 1.5) * Math.random(); //values from 1.5 to 5.0
+
+    for(int i = 0; i < bi.getWidth(); i++) {
+        for(int j = 0; j < bi.getHeight(); j++) {
+            Color c = new Color(bi.getRGB(i, j));
+            int red = (int) contrast * c.getRed() + brightness;
+            int green = (int) contrast * c.getGreen() + brightness;
+            int blue = (int) contrast * c.getBlue() + brightness;
+
+            if(red > 255) { // the values of the color components must be between 0-255
+                red = 255;
+            } else if(red < 0) {
+                red = 0;
+            }
+            if(green > 255) {
+                green = 255;
+            } else if(green < 0) {
+                green = 0;
+            }
+            if(blue > 255) {
+                blue = 255;
+            } else if(blue < 0) {
+                blue = 0;
+            }
+            imageAltered.setRGB(i, j, new Color(red, green, blue).getRGB());
+        }
+    }
+    bi = imageAltered;
+    ii = new ImageIcon(bi);
+    ImagePanel.setIcon(ii);
     }//GEN-LAST:event_ContrastActionPerformed
 
     /*
